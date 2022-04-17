12f700679183fbb728ebe0cec13edf6ff597a304
Paul Draghicescu <pdraghicescu@xevo.com>
2022-04-17 11:51:27 -0700

    wip gammonground

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-FILELIST="gnubg_web.html help.html graphics.js"
+FILELIST="gnubg_web.html help.html graphics.js webassembly.js gammonground.js"
 mkdir -p build
 emcc gnubg/*.c gnubg/lib/*.c glib/glib-2.62.0/glib/*.c glib/glib-2.62.0/glib/libcharset/*.c -O2 -o build/gnubg.js --preload-file packaged_files@/ -s 'EXPORTED_RUNTIME_METHODS=["getValue", "setValue"]' -s ALLOW_MEMORY_GROWTH=1 -DGLIB_COMPILATION=1 -DWEB=1 -I glib/glib-2.62.0/glib/ -I glib/glib-2.62.0/ -I glib/glib-2.62.0/_build/glib -I gnubg/lib/ -I gnubg/ -I glib/glib-2.62.0/_build/ -I glib/glib-2.62.0/glib/libcharset/
 
